bec8b89c6bb1c0e39fd012127e17a95489d45b04
EmersonTeles <emersonteles21@gmail.com>
2023-03-06 16:13:23 -0300

    adding dotenv and static image paths

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import db from "./db/config/db";
 import routes from "./routes";
+import { urlencoded } from "body-parser";
+import * as dotenv from "dotenv";
+import path from "path";
 const port = process.env.PORT || 8000;
 
 const app = express();
@@ -8,7 +11,18 @@ const app = express();
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+app.use(urlencoded({ limit: "1mb", extended: true }));
+dotenv.config();
 routes(app);
+// static files from public/upload
+console.log(path.resolve(__dirname, "public", "upload"));
+app.use("/upload/profile", express.static(path.resolve(__dirname, "public", "upload", "profile")));
+app.use(
+  "/upload/property",
+  express.static(path.resolve(__dirname, "public", "upload", "property"))
+);
+
 (async () => {
   try {
     await db.authenticate();
